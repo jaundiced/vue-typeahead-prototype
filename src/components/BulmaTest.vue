c023a190a1ad38a8afe3a2c9845357bef2a10bdf
jaundiced <brian_c_ackley@hotmail.com>
2018-01-01 14:41:39 -0800

    Catch repo up with local changes
    Didn't init locally vs git.  trying to sync so I can pull it down later.  Bulma typeahead needs array of strings, not objects - so not usable at the moment.

---
+++ src/components/BulmaTest.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="demo">
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Vue Bulma Typeahead Demo
+          </h1>
+        </div>
+      </div>
+    </section>
+
+    <div class="container">
+
+      <div class="columns">
+        <div class="column is-4 is-offset-4">
+          <label class="label">U.S. State</label>
+          <p class="control has-icons-left">
+            <!-- Start Typeahead Component -->
+            <typeahead :source="vendors" :onSelect="onSelect" :onChange="onChange" :limit="5" id="vendorListTypeahead" item-key="id"></typeahead>
+            <!-- End Typeahead Component -->
+            <span class="icon is-small is-left">
+              <i class="fa fa-magic"></i>
+            </span>
+          </p>
+        </div>
+      </div>
+
+      <div class="columns">
+        <div class="column is-4 is-offset-4">
+          You are searching for '{{ value }}'
+        </div>
+      </div>
+
+    </div>
+     <data-service />
+  </div>
+</template>
+  
+
+<script>
+import Typeahead from './Typeahead.vue'
+import DataService from "@/components/DataService"
+
+export default {
+  name: 'BulmaTest',
+  components: { Typeahead, DataService },
+  data () {
+    return {
+      vendors: [],
+      contract: [],
+      contractFilters: [],
+      value: ''
+    }
+  },
+  methods: {
+    onSelect (value) {
+      console.log(`'${value}' was selected.`)
+      this.value = value
+    },
+    onChange (value) {
+      console.log(`Value changed to ${value}.`)
+      this.value = value
+    }
+  },
+  created: function() {
+    this.vendors = DataService.data().vendors;
+    this.contracts = DataService.data().contracts;
+    this.contractFilters = DataService.data().contractFilters;
+  }
+}
+</script>
+
+<style scoped>
+.hero {
+  margin-bottom: 35px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Test from '@/components/Test'
+import BulmaTest from '@/components/BulmaTest'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Test',
       component: Test
+    },
+    {
+      path:"/b",
+      name: "Bulma Test",
+      component: BulmaTest
     }
   ]
 })
